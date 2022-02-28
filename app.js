36b2aacf78fed0463f066dc4e5677acda9afea22
Mark Zimmerman <markzimmerman1015@gmail.com>
2022-02-28 07:43:53 -0500

    Add random word API

--- app.js
+++ app.js
@@ -11,14 +11,16 @@ let change_colors = document.querySelector('.change_light_color');
 let colors_input = document.querySelector('.colors_input');
 let keyboard_lights = document.querySelector('.keyboard_lights');
 let keyboard_wrapper = document.querySelector('.keyboard_wrapper');
+let random_word = document.querySelector('.word-display');
 
+//sets attributes for each key//
 for(let i = 0; i < keys.length; i++) {
     keys[i].setAttribute('keyname', keys[i].innerText);
     keys[i].setAttribute('lowerCaseName', keys[i].innerText.toLowerCase());
 
 }
 
-
+//keydown functions//
 window.addEventListener('keydown', function(e) {
     for(let i = 0; i < keys.length; i++) {
         if(e.key == keys[i].getAttribute('keyname') || e.key == keys[i].getAttribute('lowerCaseName')) {
@@ -41,6 +43,7 @@ window.addEventListener('keydown', function(e) {
 
 });
 
+//Key up functions//
 window.addEventListener('keyup', function(e) {
     for(let i = 0; i < keys.length; i++) {
         if(e.key == keys[i].getAttribute('keyname') || e.key == keys[i].getAttribute('lowerCaseName')) {
@@ -50,6 +53,7 @@ window.addEventListener('keyup', function(e) {
         if(e.code == 'Space') {
             spaceKey.classList.remove('active');
             spaceKey.classList.add('remove');
+            
         }
         if(e.code == 'ShiftRight') {
             shift_right.classList.remove('active');
@@ -69,6 +73,7 @@ window.addEventListener('keyup', function(e) {
 
 });
 
+//Sets night mode//
 night_mode.addEventListener('click', function() {
     toggle_circle.classList.toggle('active');
     body.classList.toggle('active');
@@ -80,10 +85,31 @@ night_mode.addEventListener('click', function() {
         keys[i].classList.toggle('keys_night');
     }
 });
-
+//changes custom background lights//
 colors_input.addEventListener('input', function() {
     for ( let i =0; i < keys.length; i++) {
         keys[i].style.color = colors_input.value
     }
     keyboard_lights.style.background = colors_input.value;
-})
+});
+
+//Fetches random word and displays it//
+window.addEventListener('keydown', function(event) {
+    if (event.code === 'Space' ) {
+        fetch('https://random-word-api.herokuapp.com/word?number=1')
+                .then(response => response.json())
+                .then(data => random_word.innerHTML = data);
+        document.querySelector('.text').value = "";
+    }
+    
+});
+
+
+//Disables enter key default function//
+document.addEventListener('keypress', function (e) {
+    if (e.keyCode === 13 || e.which === 13) {
+        e.preventDefault();
+        return false;
+    }
+});
+
